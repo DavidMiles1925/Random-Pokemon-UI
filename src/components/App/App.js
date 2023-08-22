dd1e068425aff2defea29e4aebcc55ee66815ab6
David Miles <davidmiles1925@gmail.com>
2023-08-21 22:16:05 -0500

    drafted app ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,31 @@
+//React
+import RandomGenerator from "../RandomGenerator/RandomGenerator";
+import { useEffect, useState } from "react";
+
 // Styles
+import "../../fonts/fonts.css";
 import "./App.css";
 
 // Components
 import Header from "../Header/Header";
 
+import mockServer from "../../mockServer/mockServer";
+
 function App() {
+  const [pokemonData, setPokemonData] = useState({});
+
+  function getPokemonData() {
+    setPokemonData(mockServer);
+  }
+  //
+  useEffect(() => {
+    getPokemonData();
+  }, []);
+
   return (
     <div className='App'>
-      <Header className='app_header' />
+      <Header />
+      <RandomGenerator pokemonData={pokemonData} />
     </div>
   );
 }
---
+++ src/components/RandomGenerator/RandomGenerator.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import mockServer from "../../mockServer/mockServer";
+import "./RandomGenerator.css";
+
+function RandomGenerator({ pokemonData }) {
+  const [entryData, setEntryData] = useState({});
+  const [imageNumber, setImageNumber] = useState("");
+
+  function generateRandom() {
+    const randomNumber = Math.floor(Math.random() * 1008);
+    return randomNumber;
+  }
+
+  function getData() {
+    const randomNumber = generateRandom();
+    console.log(randomNumber);
+
+    const data = mockServer[randomNumber];
+    const stringToUse =
+      process.env.PUBLIC_URL + `/sprites/${randomNumber + 1}.jpg`;
+
+    setImageNumber(stringToUse);
+
+    setEntryData(data);
+  }
+  return (
+    <div className='page-wrapper'>
+      <div className='rand'>
+        <button className='rand__button' onClick={getData}>
+          New Pokemon!
+        </button>
+        <h1 className='rand__heading'>{entryData.name}</h1>
+        <p className='rand__attribute'></p>
+        <img
+          className='rand__sprite'
+          src={process.env.PUBLIC_URL + imageNumber}
+          alt='sprite'
+        ></img>
+        <div className='rand__info-wrapper'>
+          <p className='rand__attribute-title'>Pokedex Number: </p>
+          <p className='rand__attribute'>{entryData.number}</p>
+          <p className='rand__attribute-title'>Type 1:</p>
+          <p className='rand__attribute'>{entryData.type_0}</p>
+          <p className='rand__attribute-title'>Type 2:</p>
+          <p className='rand__attribute'>{entryData.type_1}</p>
+          <p className='rand__attribute-title'>Description: </p>
+          <p className='rand__attribute'>{entryData.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RandomGenerator;
