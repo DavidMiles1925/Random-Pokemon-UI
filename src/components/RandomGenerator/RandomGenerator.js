61faf83963321695f5f6373bc3fba7586fab412f
David Miles <davidmiles1925@gmail.com>
2023-08-22 15:02:10 -0500

    add type medallions

--- src/components/RandomGenerator/RandomGenerator.js
+++ src/components/RandomGenerator/RandomGenerator.js
@@ -5,6 +5,12 @@ import "./RandomGenerator.css";
 function RandomGenerator({ pokemonData }) {
   const [entryData, setEntryData] = useState({});
   const [imageNumber, setImageNumber] = useState("");
+  const [typeMedallion_1, setTypeMedallion_1] = useState("");
+  const [typeMedallion_2, setTypeMedallion_2] = useState("");
+
+  function firstToLower(word) {
+    return word.charAt(0).toLowerCase() + word.slice(1);
+  }
 
   function generateRandom() {
     const randomNumber = Math.floor(Math.random() * 1008) + 1;
@@ -17,9 +23,15 @@ function RandomGenerator({ pokemonData }) {
 
     const data = mockServer[randomNumber];
     const stringToUse = "/official-artwork/" + (randomNumber + 1) + ".png";
-    console.log(stringToUse);
+    const typeString_1 = `/types/${firstToLower(data.type_0)}-m.png`;
+    let typeString_2 = "";
+    data.type_1
+      ? (typeString_2 = `/types/${firstToLower(data.type_1)}-m.png`)
+      : (typeString_2 = "");
 
     setImageNumber(stringToUse);
+    setTypeMedallion_1(typeString_1);
+    setTypeMedallion_2(typeString_2);
 
     setEntryData(data);
   }
@@ -44,10 +56,30 @@ function RandomGenerator({ pokemonData }) {
         <div className='rand__info-wrapper'>
           <p className='rand__attribute-title'>Pokedex Number: </p>
           <p className='rand__attribute'>{entryData.number}</p>
-          <p className='rand__attribute-title'>Type 1:</p>
-          <p className='rand__attribute'>{entryData.type_0}</p>
-          <p className='rand__attribute-title'>Type 2:</p>
-          <p className='rand__attribute'>{entryData.type_1}</p>
+
+          <div className='rand__type-wrapper'>
+            <p className='rand__attribute-title'>Type 1:</p>
+            <img
+              className='rand__type-emblem'
+              src={process.env.PUBLIC_URL + typeMedallion_1}
+              alt='type1'
+            />
+            <p className='rand__attribute_type_type'>{entryData.type_0}</p>
+          </div>
+          {entryData.type_1 ? (
+            <div className='rand__type-wrapper'>
+              <p className='rand__attribute-title'>Type 2:</p>
+              <img
+                className='rand__type-emblem'
+                src={process.env.PUBLIC_URL + typeMedallion_2}
+                alt='type1'
+              />
+              <p className='rand__attribute_type_type'>{entryData.type_1}</p>
+            </div>
+          ) : (
+            ""
+          )}
+
           <p className='rand__attribute-title'>Description: </p>
           <p className='rand__attribute'>{entryData.description}</p>
         </div>
